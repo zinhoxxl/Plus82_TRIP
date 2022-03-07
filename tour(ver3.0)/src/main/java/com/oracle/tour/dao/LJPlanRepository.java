0b9aefbec42e77a05a0ff9dc3891b615a75ada64
zinhoxxl <zinhoxxl@gmail.com>
2022-03-07 15:18:44 +0900

    planRepository @Transactional 처리

--- tour(ver3.0)/src/main/java/com/oracle/tour/dao/LJPlanRepository.java
+++ tour(ver3.0)/src/main/java/com/oracle/tour/dao/LJPlanRepository.java
@@ -18,8 +18,8 @@ import java.util.List;
 public interface LJPlanRepository extends JpaRepository<Plan, Long> {
     List<Plan> findAllByPlannerNoAndDayBetweenOrderByPlanNo(Long plannerNo, Date start,Date end);
     List<Plan> findAllByPlannerNoOrderByPlanNo(Long plannerNo);
-    @Transactional
+    @Transactional // 삭제후 커밋처리 되야하기 때문에 이를 자동으로 수행 해주는 Transactional 어노테이션 사용
     void deleteAllByPlannerNo(Long plannerNo);
-    @Transactional
+    @Transactional // 삭제후 커밋처리 되야하기 때문에 이를 자동으로 수행 해주는 Transactional 어노테이션 사용
     void deleteByPlanNo(Long planNo);
 }
